4e6381372e8a4f7d0658b8a785d5bb87914af45d
Jet Kan <jetkan.yk@gmail.com>
2022-03-12 20:25:39 +0800

    Add day 3

--- day3/grabpanda-3.cpp
+++ day3/grabpanda-3.cpp
@@ -1,8 +1,8 @@
 /**
  * Day 3 (Order) Learning Outcomes
- *  - 
- *  - 
- *  - 
+ *  - Interactive I/O
+ *  - define vs const string
+ *  - escape character
  */
 #include <algorithm>
 #include <iostream>
@@ -14,7 +14,7 @@ typedef vector<string> vs;
 
 #define BULLET " - "
 #define DIVIDER "\n===================================================\n"
-#define INPUT " > "
+#define INPUT_SYMBOL " > "
 
 int main() {
     // Initialize database
@@ -33,12 +33,14 @@ int main() {
     getline(cin, username);
     cout << endl;
 
+    const string INPUT = " " + username + INPUT_SYMBOL;
+
     // Repeat this loop until the program terminates
     while (true) {
         // Get user input
         cout << "What would you like to do today?" << endl
              << "[browse / order / status / exit]" << endl
-             << " " + username + INPUT;
+             << INPUT;
         string user_input;
         getline(cin, user_input);
 
@@ -61,7 +63,43 @@ int main() {
             }
             result = "All menu displayed.";
         } else if (user_input == "order") {
-            result = "Execute order...";
+            // Get restaurant
+            cout << "Which restaurant would you like to order from?" << endl
+                 << INPUT;
+            string restaurant;
+            getline(cin, restaurant);
+
+            if (all_menu.find(restaurant) == all_menu.end()) {
+                // Restaurant not found
+                result = "Restaurant \"" + restaurant + "\" not found.";
+            } else {
+                // Get item
+                cout << "Which item would you like to order?" << endl
+                     << INPUT;
+                string item;
+                getline(cin, item);
+
+                vs menu = all_menu[restaurant];
+                if (find(menu.begin(), menu.end(), item) == menu.end()) {
+                    // Item not found
+                    result = "Item \"" + item + "\" not found.";
+                } else {
+                    // Get quantity
+                    cout << "How many \"" + item + "\" would you like to order?" << endl
+                         << INPUT;
+                    string quantity_str;
+                    getline(cin, quantity_str);
+                    int quantity = stoi(quantity_str);
+
+                    if (quantity <= 0) {
+                        // Invalid quantity number
+                        result = "Invalid quantity: " + quantity_str;
+                    } else {
+                        // Show confirmation message
+                        result = "Order confirmed: " + restaurant + " - " + item + " (" + quantity_str + ")";
+                    }
+                }
+            }
         } else if (user_input == "status") {
             result = "Execute status...";
         } else if (user_input == "exit") {
